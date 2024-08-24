ae9c3b4bb5f4b5df69acc33cb8490bf9676b2eed
Arthur Willyams <arthurwillyams13@gmail.com>
2024-08-24 18:38:15 -0300

    mapeando

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,10 +1,36 @@
+using Dima.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var cnnStr = builder
+    .Configuration
+    .GetConnectionString("DefaultConnection") ?? string.Empty;
+
+builder.Services.AddDbContext<AppDbContext>(
+    x =>
+    {
+        x.UseSqlServer();
+    }
+);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x =>
+{
+    x.CustomSchemaIds(n => n.FullName);
+});
+builder.Services.AddTransient<Handler>();
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 
 app.MapPost(
         "/v1/transactions",
-        (Request request, Handler handler) 
+
+        (Request request, Handler handler)
         => handler.Handle(request))
     .WithSummary("Cria uma nova transação")
     .WithName("Transactions:Create")
--- Dima.Api/obj/Debug/net8.0/Dima.Api.AssemblyInfo.cs
+++ Dima.Api/obj/Debug/net8.0/Dima.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dima.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c6508d0379275abc8a79ed59242c2273025990f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dima.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dima.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
